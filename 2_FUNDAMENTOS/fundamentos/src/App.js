8835812a967a776a3d093acafd72a87443d8c0d5
Cauane Cardoso <cauane.cardoso.rosa@gmail.com>
2023-04-13 22:10:25 -0300

    Initial commit

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,16 @@
+//components
+import FirstComponent from "./components/FirstComponent"
+// styles / css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Fundamentos do React</h1>
+    <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
+
